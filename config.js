ec658f559ab691610dd4dff7d554e95ab58c2795
ProfEdgarH <edhemo@gmail.com>
2026-06-23 13:39:54 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuración de Supabase
-// Este archivo contiene las credenciales de Supabase
-// NO subir este archivo a GitHub
-
-const SUPABASE_CONFIG = {
-  URL: "https://zisrjgkhyczbxkbolupz.supabase.co",
-  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc3JqZ2toeWN6YnhrYm9sdXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NjkzNzcsImV4cCI6MjA5NjM0NTM3N30.8ZIjik6gGCICuorGHinTeBOJKxCNw7J1wGZ-2zYJ_u0"
-};
